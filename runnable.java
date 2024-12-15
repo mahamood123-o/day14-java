5d461c0c3d74e4f3338bed9610111b0a12d04362
Mahamood <shaikmisba305@gmail.com>
2024-12-15 10:31:59 +0530

    Add files via upload

---
+++ runnable.java
@@ -0,0 +1,18 @@
+//implementing runnable interfaces
+class a implements Runnable
+{
+    public void run()
+    {
+        System.out.println(" Hello I'm Thread");
+    }
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        a r=new a();
+        Thread s=new Thread(r);
+        s.start();
+        s.run();
+    }
+}
