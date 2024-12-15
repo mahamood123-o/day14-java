66b11eb5df12ca9a6e437764de712e1d85f4bc11
Mahamood <shaikmisba305@gmail.com>
2024-12-15 10:27:21 +0530

    Add files via upload

---
+++ thread.java
@@ -0,0 +1,17 @@
+// Extending Thread class
+class a extends Thread
+{
+    public void r()
+    {
+        System.out.println(" Hello I'm Thread");
+    }
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        a g=new a();
+        g.r();
+        g.start();
+    }
+}
